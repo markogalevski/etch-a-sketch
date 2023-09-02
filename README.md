8b02d59705244b819341213d1f5cb247c5f95dae
Marko Galevski <galevski.marko+github@gmail.com>
2023-09-02 22:11:31 +0200

    added mode switchability, added various modes, made slightly sexier, but not htat great. Still, project done

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## Feature List:
 [x] Drag the mouse around in a 16x16 grid, paints black
-[ ] Resizeable grid over the same pixel space (up to 100x100)
+[x] Resizeable grid over the same pixel space (up to 100x100)
 [x] Rainbow mode! (pastel flavour for funsies)
 [x] Shading mode!
-[ ] Make the modes actually switchable
-[ ] Reset button!
+[x] Make the modes actually switchable
+[x] Reset button!
 [ ] Make it sexy
--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,50 +1,27 @@
-const container = document.querySelector(".container");
-console.log(container);
+const default_size = 16;
+init(default_size);
 
-const GRID_SIDE = 16;
-let i = 0;
-let j = 0;
-for (i = 0; i < GRID_SIDE; i++) {
-  const row_container = document.createElement('div');
-  row_container.classList.add('row_container');
-  for (j = 0; j < GRID_SIDE; j++) { 
-      const div = document.createElement('div');
-      div.style.backgroundColor = 'white';
-      div.classList.add('grid_square');
-      if (j == 0) {
-        div.classList.add('left_edge');
-      } 
-      if (j == GRID_SIDE - 1) {
-        div.classList.add('right_edge');
-      } 
-      if (i == 0) {
-        div.classList.add('top_edge');
-      } 
-      if (i == GRID_SIDE - 1) {
-        div.classList.add('bottom_edge');
-      } 
-      row_container.appendChild(div);
-    }
-  container.appendChild(row_container);
-}
-
-const grid_squares = document.querySelectorAll('.grid_square');
-grid_squares.forEach(grid_square => {
-  grid_square.addEventListener('mouseenter', rainbow_mode);
-})
+const resize_button = document.querySelector('.resize');
+resize_button.addEventListener('click', resize);
+const rainbow_button = document.querySelector('.rainbow');
+rainbow_button.addEventListener('click', rainbow);
+const shading_button = document.querySelector('.shading');
+shading_button.addEventListener('click', shading);
+const black_button = document.querySelector('.black');
+black_button.addEventListener('click', black);
+const reset_button = document.querySelector('.clear');
+reset_button.addEventListener('click', paint_white);
 
 
 function black_mode(e) {
   const grid_square = e.target;
   let new_color = 'black'
-  grid_square.style['background-color'] = new_color;
+  grid_square.style.backgroundColor = new_color;
 }
-  
-
 
 function shading_mode(e) {
   const grid_square = e.target;
-  let current_color = grid_square.style['background-color'];
+  let current_color = grid_square.style.backgroundColor;
   let new_color;
   if (current_color == 'white') {
     new_color = 'rgba(1, 1, 1, 0.1)';
@@ -55,7 +32,7 @@ function shading_mode(e) {
     new_color = `rgba(1, 1, 1, ${alpha + 0.1})`;
     
   }
-  grid_square.style['background-color'] = new_color;
+  grid_square.style.backgroundColor = new_color;
 }
 
 function rainbow_mode(e) {
@@ -75,4 +52,81 @@ function clamp(number, lower, upper) {
     let result = Math.max(number, lower);
     result = Math.min(result, upper);
     return result;
+}
+
+function resize(_e) {
+  let grid_size = clamp(parseInt(prompt("Enter the new grid size!")), 1, 100);
+  grid_size = grid_size > 1 ? grid_size : 16;
+  init(grid_size);
+}
+
+function rainbow(_e) {
+  paint_white();
+  select_mode(rainbow_mode);
+}
+
+function shading(e) {
+  paint_white();
+  select_mode(shading_mode);
+}
+
+function black(_e) {
+  paint_white();
+  select_mode(black_mode);
+}
+
+
+function init(grid_size) {
+  const container = document.querySelector(".container");
+  while (container.lastChild) {
+    container.removeChild(container.lastChild);
+  }
+
+  let i = 0;
+  let j = 0;
+  for (i = 0; i < grid_size; i++) {
+    const row_container = document.createElement('div');
+    row_container.classList.add('row_container');
+    for (j = 0; j < grid_size; j++) { 
+        const div = document.createElement('div');
+        div.style.backgroundColor = 'white';
+        div.classList.add('grid_square');
+        if (j == 0) {
+          div.classList.add('left_edge');
+        } 
+        if (j == grid_size - 1) {
+          div.classList.add('right_edge');
+        } 
+        if (i == 0) {
+          div.classList.add('top_edge');
+        } 
+        if (i == grid_size - 1) {
+          div.classList.add('bottom_edge');
+        } 
+        row_container.appendChild(div);
+      }
+    container.appendChild(row_container);
+  }
+  select_mode(black_mode);
+}
+
+function select_mode(mode_func) {
+  const grid_squares = document.querySelectorAll('.grid_square');
+  grid_squares.forEach(grid_square => {
+    remove_all_listeners(grid_square);
+    grid_square.addEventListener('mouseenter', mode_func);
+  })
+}
+
+function paint_white() {
+  const grid_squares = document.querySelectorAll('.grid_square');
+  grid_squares.forEach(grid_square => {
+    grid_square.style.backgroundColor = 'white';
+  })
+}
+
+function remove_all_listeners(node) {
+  node.removeEventListener('mouseenter', rainbow_mode);
+  node.removeEventListener('mouseenter', shading_mode);
+  node.removeEventListener('mouseenter', black_mode);
 }
